3974809d3204f973dbb5311d9d34ac5c2a3e894c
AlexKat <katsubo79@gmail.com>
2023-10-20 03:39:54 +0300

    2010

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
 
 console.log(fetchBreeds());
-console.log(fetchCatByBreed('abys'));
+console.log(fetchCatByBreed('char'));
 
 refs = {
     select: document.querySelector('.breed-select'),
@@ -10,21 +10,66 @@ refs = {
     catInfo: document.querySelector('.cat-info'),
 }
 
-// refs.select.addEventListener('change', onCatalog);
-
-
-
 fetchBreeds()
-    .then((data) => refs.select.innerHTML = createMarkup(data)
+    .then((data) => {
+         console.log(data);
+        refs.select.innerHTML = createMarkup(data);
+    }
     )
     .catch((err) => console.log(err));
-
-
-
-
-
 
 function createMarkup(arr) {
      return arr.map(({id, name }) =>
      `<option value="${id}">${name}</option>`).join("");
+}
+
+refs.select.addEventListener('change', onCatalog);
+function onCatalog(e) {
+    console.log(e.target.value);
+
+    fetchCatByBreed(e.target.value)
+        
+        .then((dataCat) => {
+            console.log(dataCat);
+            refs.catInfo.innerHTML = createCatInfo(dataCat);
+        
+    })
+    .catch((err) => console.log(err));
+
+};
+
+
+
+// function createCatInfo(dataCat) {
+
+//     console.log(data[0]);
+//     const {
+//       url,
+//   } = dataCat[0];
+
+//   const {
+//        description,
+//        temperament,
+    
+//   } = data[0];
+
+ 
+//     return `<img src="${url}" alt="" width='300'>
+//       <h2 class="h-name"></h2>
+//       <h3 class="h-origin"></h3>
+//       <p class="p-description">${description}</p>
+//       <p class="p-temperament">${temperament}</p>`
+// }
+
+
+function createCatInfo(e) {
+    return e.map(({
+        id, url, data,
+     name, description, temperament
+}) =>
+     `<img src="${url}" alt="${name}" width='300'>
+      <h2 class="h-name">${name}</h2>
+      <h3 class="h-origin"></h3>
+      <p class="p-description">${description}</p>
+      <p class="p-temperament">${temperament}</p>`);
 }
