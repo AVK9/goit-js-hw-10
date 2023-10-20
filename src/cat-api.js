8a2cd1b11d75a95970b08b1048031b3607ee5660
AlexKat <katsubo79@gmail.com>
2023-10-20 14:27:12 +0300

    +

--- src/cat-api.js
+++ src/cat-api.js
@@ -5,7 +5,7 @@ import axios from "axios";
 axios.defaults.headers.common["x-api-key"] = `${API_KEY}`;
 
 
-function fetchBreeds(id, name) {
+function fetchBreeds() {
  return fetch(`${BASE_URL}/breeds`)
     .then((resp) => {
     // console.log(resp);
@@ -14,8 +14,14 @@ function fetchBreeds(id, name) {
     }
     return resp.json();
     })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
-
+// limit=10&
 function fetchCatByBreed(breedId){
  return fetch(`${BASE_URL}/images/search?breed_ids=${breedId}`)
     .then((resp) => {
@@ -24,10 +30,33 @@ function fetchCatByBreed(breedId){
       throw new Error(resp.statusText);
     }
     return resp.json();
-  });
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
 };
 
-export {fetchBreeds, fetchCatByBreed};
+function fetchCatInfo(id){
+ return fetch(`${BASE_URL}/images/${id}`)
+    .then(response => {
+      // console.log(response);
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .then(data => {
+      return data;
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+export {fetchBreeds, fetchCatByBreed, fetchCatInfo};
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import { fetchBreeds, fetchCatByBreed, fetchCatInfo} from './cat-api';
 
 console.log(fetchBreeds());
 console.log(fetchCatByBreed('char'));
+console.log(fetchCatInfo('0XYvRd7oD'));
 
 refs = {
     select: document.querySelector('.breed-select'),
@@ -12,64 +13,59 @@ refs = {
 
 fetchBreeds()
     .then((data) => {
-         console.log(data);
+        console.log(data);
         refs.select.innerHTML = createMarkup(data);
     }
     )
     .catch((err) => console.log(err));
 
 function createMarkup(arr) {
-     return arr.map(({id, name }) =>
+     return arr.map(({id, name}) =>
      `<option value="${id}">${name}</option>`).join("");
 }
 
 refs.select.addEventListener('change', onCatalog);
 function onCatalog(e) {
-    console.log(e.target.value);
 
+    console.log(e.target.value);
     fetchCatByBreed(e.target.value)
-        
         .then((dataCat) => {
             console.log(dataCat);
+            const { id, url } = dataCat[0]
             refs.catInfo.innerHTML = createCatInfo(dataCat);
-        
-    })
-    .catch((err) => console.log(err));
-
-};
-
-
-
-// function createCatInfo(dataCat) {
-
-//     console.log(data[0]);
-//     const {
-//       url,
-//   } = dataCat[0];
-
-//   const {
-//        description,
-//        temperament,
+            //  refs.catInfo.insertAdjacentHTML('beforeend', markupCatInfo);
+    console.log(id);
+    fetchCatInfo(id)
+        .then((dataCatInfo) => {
+            console.log(dataCatInfo);
+            
+            // refs.catInfo.innerHTML = createCatInfo(dataCat);
+            
+             refs.catInfo.insertAdjacentHTML('beforeend', createCatInfo2(dataCatInfo));
     
-//   } = data[0];
+        })
+        .catch((err) => console.log(err));
+   
+        })
+        .catch((err) => console.log(err));
+}
 
+function createCatInfo(dataCat) {
+
+    console.log(dataCat[0]);
+    const {
+      id,
+      url,
+    } = dataCat[0];
  
-//     return `<img src="${url}" alt="" width='300'>
-//       <h2 class="h-name"></h2>
-//       <h3 class="h-origin"></h3>
-//       <p class="p-description">${description}</p>
-//       <p class="p-temperament">${temperament}</p>`
-// }
+    return `<img src="${url}" alt="${id}" width='300'>`
+}
 
-
-function createCatInfo(e) {
-    return e.map(({
-        id, url, data,
-     name, description, temperament
-}) =>
-     `<img src="${url}" alt="${name}" width='300'>
-      <h2 class="h-name">${name}</h2>
-      <h3 class="h-origin"></h3>
-      <p class="p-description">${description}</p>
-      <p class="p-temperament">${temperament}</p>`);
+function createCatInfo2(dataCatInfo) {
+const { breeds: [{name, description, temperament }]} = dataCatInfo;
+    return `<h2 class="h-name">${name}</h2>
+    <h3 class="h-origin"></h3>
+    <p class="p-description">${description}</p>
+    <p class="p-temperament">${temperament}</p>`
+     
 }
